072b6c8ba41407c7158c9ce03442482096a25d2e
Clement Hoang <clement.hoang24@gmail.com>
2016-04-18 13:41:26 -0400

    Add random quotes to top right corner

--- js/main.js
+++ js/main.js
@@ -14,6 +14,23 @@ $(function() {
     });
 });
 
+$(function() {
+  var QUOTES = [{
+    quote: "You can't climb the ladder of success with your hands in your pockets.",
+    author: "&mdash;Arnold Schwarzenegger"
+  },
+  {
+    quote: "Only those who will risk going too far can possibly find out how far one can go.",
+    author: "&mdash;T. S. Eliot"
+  }];
+
+  var randomNumber = Math.floor(Math.random() * (QUOTES.length));
+  var randomQuote = QUOTES[randomNumber];
+
+  $('.quotes>.quote').html(randomQuote.quote);
+  $('.quotes>.author').html(randomQuote.author);
+});
+
 // $(function(){
 //     $(".heading").typed({
 //         strings: ["Hey there,", "good lookin'", ";)"],

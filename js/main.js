1d408e84878593f6648b1a297c6659db64ce0a16
Clement Hoang <clement.hoang24@gmail.com>
2017-05-08 16:00:15 -0400

    Add quote

--- js/main.js
+++ js/main.js
@@ -22,10 +22,12 @@ $(function() {
   var QUOTES = [{
     quote: "You can't climb the ladder of success with your hands in your pockets.",
     author: "&mdash;Arnold Schwarzenegger"
-  },
-  {
+  }, {
     quote: "Only those who will risk going too far can possibly find out how far one can go.",
     author: "&mdash;T. S. Eliot"
+  }, {
+    quote: "Tell me what you eat and I will tell you what you are.",
+    author: "&mdash;Anthelme Brillat-Savarin"
   }];
 
   var randomNumber = Math.floor(Math.random() * (QUOTES.length));
